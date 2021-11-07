359d527b61e5d2ac78deca3b097ffcb7ddac15fd
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-11-07 23:12:45 +0200

    Setting up API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const App = () => {
   return (
--- src/api.js
+++ src/api.js
@@ -1,2 +1,26 @@
 // BASE URL
-const base_url = `https://api.rawg.io/api/games?key=${process.env.process.env.REACT_APP_API_KEY}`;
+const base_url = `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`;
+
+// Get date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  return month < 10 ? `0${month}` : month;
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  return day < 10 ? `0${day}` : day;
+};
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Popular games
+const popularGames = `&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popularGames}`;
